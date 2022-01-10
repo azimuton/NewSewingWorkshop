e41661c14fc0c3ee4e9dbece341fae630254897f
ArtemZonov <artemzonov21@gmail.com>
2022-01-11 00:10:59 +0200

    added main ui

--- app/src/main/java/com/example/newsewingworkshop/MainActivity.kt
+++ app/src/main/java/com/example/newsewingworkshop/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.newsewingworkshop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        Glide.with(this)
+//            .asGif()
+//            .load(R.drawable.sdfff)
+//            .into(button);
+
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
+
+
+
+
+
+
